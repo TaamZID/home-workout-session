166abcfdd482befc644a7df7d73f2e0ad08def22
TaamZID <tamjidalam00@gmail.com>
2022-09-29 11:58:12 +0600

    break-done

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,17 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Details.css";
 
 const Details = ({ cart }) => {
-//   const { cart } = props.cart;
+  const [cnt, setBreak] = useState(0);
 
   let total = 0;
   for (const exercise of cart) {
     total = parseFloat(total + exercise.time);
   }
+
   return (
     <div className="details">
       <h4>Exercise Details</h4>
-      <p>Exercise Time: {total}s</p>
+      <div>
+        <h4>Add a break</h4>
+        <div className="break">
+          <button
+            onClick={() => {
+              setBreak(10);
+            }}
+          >
+            10s
+          </button>
+          <button
+            onClick={() => {
+              setBreak(20);
+            }}
+          >
+            20s
+          </button>
+          <button
+            onClick={() => {
+              setBreak(30);
+            }}
+          >
+            30s
+          </button>
+          <button
+            onClick={() => {
+              setBreak(40);
+            }}
+          >
+            40s
+          </button>
+          <button
+            onClick={() => {
+              setBreak(50);
+            }}
+          >
+            50s
+          </button>
+        </div>
+      </div>
+      <h4>Exercise Time: {total}s</h4>
+      <h4>Break Time: {cnt}s</h4>
     </div>
   );
 };
